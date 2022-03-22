fb08f9555009e37792508a513b5de41c827cee9b
Tobiasz Nogaj <tobnog@hotmail.com>
2022-03-22 13:52:18 +0100

    Set up DB connection via API

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { DbService } from "../db.service";
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.css']
 })
+
 export class DataComponent implements OnInit {
 
-  constructor() { }
+  username = 'user0001';
 
-  ngOnInit(): void {
+  user: User = {
+    id: 0,
+    p0: '',
+    p1: '',
+    p2: '',
+    p3: '',
+    p4: '',
+    p5: '',
+    p6: '',
+    p7: '',
+    p8: '',
+    p9: ''
+  };
+
+  constructor(private db: DbService) { }
+
+  getData(): void {
+    this.db.getUser().subscribe(t_user =>{
+      this.user = t_user;
+    })
   }
 
+  ngOnInit(): void {
+    this.getData();
+  }
+
+}
+
+export interface User {
+  id: number,
+  p0: string,
+  p1: string,
+  p2: string,
+  p3: string,
+  p4: string,
+  p5: string,
+  p6: string,
+  p7: string,
+  p8: string,
+  p9: string
 }
--- src/app/db.service.ts
+++ src/app/db.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { User } from './data/data.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DbService {
 
-  constructor() { }
+  host = 'https://imsi.pl:5000/player/user0001';
+
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<User> {
+    return this.http.get<User>(this.host);
+  }
 }
